03e46dad8c2874d03250d3375f1db911904e502f
Suusana <shenhanhan1@gmail.com>
2025-09-01 02:55:24 +1000

    Club list view

---
+++ Frontend/src/pages/ClubDetail.jsx
@@ -0,0 +1,6 @@
+const ClubDetail = ()=>{
+  return (
+    <div>This is the club details</div>
+  );
+}
+export default ClubDetail;
--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/home.jsx";
 
 import Friends from "../pages/Friends.jsx";
-import Clubs from "../pages/clubs.jsx";
+import Clubs from "../pages/Clubs.jsx";
 import DiscussionBoard from "../pages/DiscussionBoard.jsx";
 import Appointment from "../pages/appointment.jsx";
 import Login from "../pages/Login.jsx";
@@ -10,6 +10,7 @@ import Register from "../pages/Register.jsx";
 import Review from "../pages/Review.jsx";
 import Layout from "../Layout.jsx";
 import Timetable from "../pages/Timetable.jsx";
+import ClubDetail from "../pages/ClubDetail.jsx";
 //This is react-router-dom v7
 //This file defines all the routes in the application
 
@@ -28,12 +29,13 @@ const Router = createBrowserRouter([
     children: [
       { path: "home", element: <Home /> },
       { path: "friends", element: <Friends /> },
-      { path: "clubs", element: <Clubs /> },
+      { path: "clubs", element: <Clubs />},
+      { path: "clubs/id", element: <ClubDetail /> },
       { path: "appointment", element: <Appointment /> },
       { path: "discussion", element: <DiscussionBoard /> },
       { path: "review", element: <Review /> },
       { path: "timetable", element: <Timetable /> },
     ],
-  },
+  }
 ]);
 export default Router;
