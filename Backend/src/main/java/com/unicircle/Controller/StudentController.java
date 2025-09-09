35d4a854a540380e33248a86317feadc9d74beec
zizhu10y <sarahzhao10y@gmail.com>
2025-09-09 17:56:58 +1000

    Update

--- Backend/src/main/java/com/unicircle/Controller/StudentController.java
+++ Backend/src/main/java/com/unicircle/Controller/StudentController.java
@@ -1,23 +1,19 @@
 package com.unicircle.Controller;
 
 import com.unicircle.Bean.Student;
-import com.unicircle.Repository.StudentRepo;
 import com.unicircle.Service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class StudentController {
-    private final StudentService service;
-    public StudentController(StudentService service) {
-        this.service = service;
-    }
+    @Autowired
+    private StudentService studentService;
     @PostMapping({"/signup", "/register"})
     public ResponseEntity<Student> signup(@RequestBody Student student) {
-        return ResponseEntity.ok(service.register(student));
+        return ResponseEntity.ok(studentService.register(student));
     }
 }
