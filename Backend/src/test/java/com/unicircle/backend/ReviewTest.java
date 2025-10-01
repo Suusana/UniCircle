ee31c0995ce31a892343ce36ed2563230541b07f
zizhu10y <sarahzhao10y@gmail.com>
2025-10-01 19:26:57 +1000

    Create ReviewTest.java
    CI

---
+++ Backend/src/test/java/com/unicircle/backend/ReviewTest.java
@@ -0,0 +1,11 @@
+package com.unicircle.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReviewTest {
+    @Test
+    void smoke(){
+        assertEquals(2, 1+1);
+    }
+}
