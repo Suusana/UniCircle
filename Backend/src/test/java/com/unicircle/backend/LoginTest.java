313035f9348840de0f106eccaaa81b48709d1d04
zizhu10y <sarahzhao10y@gmail.com>
2025-10-01 19:42:51 +1000

    Create LoginTest.java

---
+++ Backend/src/test/java/com/unicircle/backend/LoginTest.java
@@ -0,0 +1,12 @@
+package com.unicircle.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginTest {
+    @Test
+    void smoke(){
+        assertEquals(2, 1+1);
+    }
+}
+
