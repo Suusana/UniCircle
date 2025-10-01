4efc4ff3dcf69cada01d01b4023eb640fcfae543
zizhu10y <sarahzhao10y@gmail.com>
2025-10-01 23:06:04 +1000

    Pipeline test

--- Backend/src/test/java/com/unicircle/backend/LoginTest.java
+++ Backend/src/test/java/com/unicircle/backend/LoginTest.java
@@ -5,8 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LoginTest {
     @Test
-    void smoke(){
-        assertEquals(2, 1+1);
+    void correctPasswordShouldLogin(){
+        String password = "123123123";
+        boolean result = "123123123".equals(password);
+        assertTrue(result);
+    }
+
+    @Test
+    void wrongPasswordShouldFail(){
+        String password = "321321321";
+        boolean result = "123123123".equals(password);
+        assertFalse(result);
     }
 }
 
