fe032bb48678aeb39702312ff5a15590d0bc5002
Suusana <shenhanhan1@gmail.com>
2025-10-12 18:23:57 +1100

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import router from './router/index.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from "./contexts/AuthContext"
-import { HashRouter } from 'react-router-dom';
 
 // Wrap the application with AuthProvider to provide authentication context
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <HashRouter>
-        <RouterProvider router={router} />
-      </HashRouter>
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>
 );
