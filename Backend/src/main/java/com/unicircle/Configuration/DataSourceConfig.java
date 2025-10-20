9dc52e25c4dc8c1dbc3038b43e1b4a3bbfa5d227
StarStriker <77044214+luckyseo@users.noreply.github.com>
2025-10-20 23:17:57 +1100

    config save

--- Backend/src/main/java/com/unicircle/Configuration/DataSourceConfig.java
+++ Backend/src/main/java/com/unicircle/Configuration/DataSourceConfig.java
@@ -7,30 +7,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-@Configuration
-public class DataSourceConfig {
-
-   @Value("${user.home}")
-   private String userHome;
-
-   @Bean
-   public DataSource dataSource() {
-       String dbPath = userHome + "/unicircle.db";
-       DriverManagerDataSource dataSource = new DriverManagerDataSource();
-       dataSource.setDriverClassName("org.sqlite.JDBC");
-       dataSource.setUrl("jdbc:sqlite:" + dbPath);
-       return dataSource;
-   }
-}
-
 // @Configuration
 // public class DataSourceConfig {
-//     @Bean
-//     public DataSource dataSource() {
-//         String dbPath = "/home/site/wwwroot/unicircle.db";
-//         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//         dataSource.setDriverClassName("org.sqlite.JDBC");
-//         dataSource.setUrl("jdbc:sqlite:" + dbPath);
-//         return dataSource;
-//     }
+
+//    @Value("${user.home}")
+//    private String userHome;
+
+//    @Bean
+//    public DataSource dataSource() {
+//        String dbPath = userHome + "/unicircle.db";
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.sqlite.JDBC");
+//        dataSource.setUrl("jdbc:sqlite:" + dbPath);
+//        return dataSource;
+//    }
 // }
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource dataSource() {
+        String dbPath = "/home/site/wwwroot/unicircle.db";
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:" + dbPath);
+        return dataSource;
+    }
+}
